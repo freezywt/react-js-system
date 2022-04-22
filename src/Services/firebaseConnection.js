dcccd1f1fbed352ca57499845f5cd519ffc1398c
freezywt <freezywt666@gmail.com>
2022-04-22 16:35:39 -0300

    firebase key removed

--- src/Services/firebaseConnection.js
+++ src/Services/firebaseConnection.js
@@ -4,13 +4,7 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 let firebaseConfig = {
-    apiKey: "AIzaSyBNqjvlYD5MVnRHYGEkPMU1NbxZ6c8JoyY",
-    authDomain: "system-21d91.firebaseapp.com",
-    projectId: "system-21d91",
-    storageBucket: "system-21d91.appspot.com",
-    messagingSenderId: "824479878009",
-    appId: "1:824479878009:web:73b24c0dbcd43af605fda8",
-    measurementId: "G-1PCSWN1T2G"
+    /* input you firebase config */
 };
 
 if(!firebase.apps.lenght){
